1ff010c9cfb3c8bfc9c1de61e54a72ce1712866c
Josh Odahara <josh.odahara@gmail.com>
2026-06-06 20:49:27 -0700

    WIP: add Tailwind styling and active tab buttons

--- lib/pokedex_web/live/pokemon_live.ex
+++ lib/pokedex_web/live/pokemon_live.ex
@@ -79,73 +79,86 @@ defmodule PokedexWeb.PokemonLive do
 
   def render(assigns) do
     ~H"""
-    <h1>Pokedex</h1>
+    <div class="min-h-screen bg-gray-1000 text-white p-8">
+      <h1 class="text-4xl font-bold text-red-500 mb-8">Pokedex</h1>
 
-    <button phx-click="switch_tab" phx-value-tab="upload">Uploader</button>
-    <button phx-click="switch_tab" phx-value-tab="browse">Browser</button>
+      <div class="flex gap-4 mb-8">
+        <button
+          phx-click="switch_tab"
+          phx-value-tab="upload"
+          class={if @active_tab == :upload, do: "px-6 py-2 rounded-full font-semibold bg-red-500 text-white", else: "px-6 py-2 rounded-full font-semibold bg-gray-700 text-white hover:bg-gray-600"}>
+          Upload
+        </button>
+        <button
+          phx-click="switch_tab"
+          phx-value-tab="browse"
+          class={if @active_tab == :browse, do: "px-6 py-2 rounded-full font-semibold bg-red-500 text-white", else: "px-6 py-2 rounded-full font-semibold bg-gray-700 text-white hover:bg-gray-600"}>
+          Browse
+        </button>
+      </div>
 
-    <div :if={@active_tab == :upload}>
-      Upload here
-      <form phx-change="validate" phx-submit="import_csv">
-        <.live_file_input upload={@uploads.csv} />
-        <button type="submit">Preview CSV</button>
-        <table>
-          <thead>
-            <tr>
-              <th>Number </th>
-              <th>Name </th>
-              <th>Nickname </th>
-              <th>Type </th>
-              <th>HP </th>
-              <th>Attack </th>
-              <th>Defense </th>
-              <th>Speed </th>
-              <th>Shiny </th>
-            </tr>
-          </thead>
+      <div :if={@active_tab == :upload}>
+        Upload here
+        <form phx-change="validate" phx-submit="import_csv">
+          <.live_file_input upload={@uploads.csv} />
+          <button type="submit">Preview CSV</button>
+          <table>
+            <thead>
+              <tr>
+                <th>Number </th>
+                <th>Name </th>
+                <th>Nickname </th>
+                <th>Type </th>
+                <th>HP </th>
+                <th>Attack </th>
+                <th>Defense </th>
+                <th>Speed </th>
+                <th>Shiny </th>
+              </tr>
+            </thead>
 
-          <tbody>
-            <tr :for={pokemon <- @preview_data}>
-              <td><%= pokemon.number %></td>
-              <td><%= pokemon.name %></td>
-              <td><%= pokemon.nickname %></td>
-              <td><%= pokemon.type %></td>
-              <td><%= pokemon.hp %></td>
-              <td><%= pokemon.attack %></td>
-              <td><%= pokemon.defense %></td>
-              <td><%= pokemon.speed %></td>
-              <td><%= pokemon.shiny %></td>
-            </tr>
-          </tbody>
-        </table>
+            <tbody>
+              <tr :for={pokemon <- @preview_data}>
+                <td><%= pokemon.number %></td>
+                <td><%= pokemon.name %></td>
+                <td><%= pokemon.nickname %></td>
+                <td><%= pokemon.type %></td>
+                <td><%= pokemon.hp %></td>
+                <td><%= pokemon.attack %></td>
+                <td><%= pokemon.defense %></td>
+                <td><%= pokemon.speed %></td>
+                <td><%= pokemon.shiny %></td>
+              </tr>
+            </tbody>
+          </table>
 
-        <button :if={@preview_data != []} type="button" phx-click="confirm_import">Confirm Import</button>
-        <div :if={@loading}>Loading...</div>
-      </form>
-    </div>
+          <button :if={@preview_data != []} type="button" phx-click="confirm_import">Confirm Import</button>
+          <div :if={@loading}>Loading...</div>
+            </form>
+          </div>
 
-    <div :if={@active_tab == :browse}>
-    <form phx-change="search">
-      <input type="search" name="search" value={@search} placeholder="Search Pokemon" phx-change="search" />
-    </form>
+      <div :if={@active_tab == :browse}>
+        <form phx-change="search">
+          <input type="search" name="search" value={@search} placeholder="Search Pokemon" phx-change="search" />
+        </form>
 
-    <div :if={@editing_pokemon}>
-      <h3>Editing <%= @editing_pokemon.name %></h3>
-      <form phx-submit="update_pokemon">
-        <input type="hidden" name="id" value={@editing_pokemon.id} />
-        <input type="number" name="number" value={@editing_pokemon.number} />
-        <input type="text" name="name" value={@editing_pokemon.name} />
-        <input type="text" name="nickname" value={@editing_pokemon.nickname} />
-        <input type="text" name="type" value={@editing_pokemon.type} />
-        <input type="number" name="hp" value={@editing_pokemon.hp} />
-        <input type="number" name="attack" value={@editing_pokemon.attack} />
-        <input type="number" name="defense" value={@editing_pokemon.defense} />
-        <input type="number" name="speed" value={@editing_pokemon.speed} />
-        <input type="checkbox" name="shiny" checked={@editing_pokemon.shiny} />
-        <button type="submit">Save</button>
-        <button type="button" phx-click="cancel_edit">Cancel</button>
-      </form>
-    </div>
+      <div :if={@editing_pokemon}>
+        <h3>Editing <%= @editing_pokemon.name %></h3>
+          <form phx-submit="update_pokemon">
+            <input type="hidden" name="id" value={@editing_pokemon.id} />
+            <input type="number" name="number" value={@editing_pokemon.number} />
+            <input type="text" name="name" value={@editing_pokemon.name} />
+            <input type="text" name="nickname" value={@editing_pokemon.nickname} />
+            <input type="text" name="type" value={@editing_pokemon.type} />
+            <input type="number" name="hp" value={@editing_pokemon.hp} />
+            <input type="number" name="attack" value={@editing_pokemon.attack} />
+            <input type="number" name="defense" value={@editing_pokemon.defense} />
+            <input type="number" name="speed" value={@editing_pokemon.speed} />
+            <input type="checkbox" name="shiny" checked={@editing_pokemon.shiny} />
+            <button type="submit">Save</button>
+            <button type="button" phx-click="cancel_edit">Cancel</button>
+        </form>
+      </div>
 
       <table>
           <thead>
@@ -181,7 +194,8 @@ defmodule PokedexWeb.PokemonLive do
             </tr>
           </tbody>
         </table>
+      </div>
     </div>
-    """
+  """
   end
 end
